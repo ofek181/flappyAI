c6b2237d5552fae53293563dc2c7bcb5d8db2285
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-26 15:22:41 +0300

    added the floor module

--- consts.py
+++ consts.py
@@ -19,3 +19,9 @@ class PipeConsts:
     VERTICAL_GAP = 150  # gap between top and bottom pipe
     HORIZONTAL_GAP = 200  # gap between pipe pairs on the x axis
     PIPE_VELOCITY = 5
+
+
+class FloorConsts:
+    FLOOR_IMAGE = None
+    FLOOR_VELOCITY = 5
+
---
+++ floor_module.py
@@ -0,0 +1,47 @@
+from consts import FloorConsts
+
+
+class Floor:
+    """
+           A class to represent the floor.
+           ----------------------------
+           Attributes
+           ----------------------------
+           floor_image : list[pngs]
+               list of three identical images of the floor to implement the movement animation
+           x : list[int]
+               x position of the three images
+           y : int
+               y position of the images
+           ----------------------------
+           Methods
+           ----------------------------
+           __init__(self, y):
+               Constructs all the necessary attributes for the floor object.
+           move(self):
+               Moves the floor with the velocity of the pipes
+    """
+    def __init__(self, y_pos: int = 500):
+        """
+            Constructs all the necessary attributes for the floor object.
+               Parameters:
+                   y_pos (int): y position of the floor
+        """
+        # three identical images of the floor will be used to animate the floor's movement.
+        self.floor_image = [FloorConsts.FLOOR_IMAGE, FloorConsts.FLOOR_IMAGE, FloorConsts.FLOOR_IMAGE]
+        self.x = [0, FloorConsts.FLOOR_IMAGE.get_width(), FloorConsts.FLOOR_IMAGE.get_width() * 2]
+        self.y = y_pos  # the y position of the floor image
+
+    def move(self):
+        """
+           Implements the movement of the floor with respect to game velocity.
+        """
+        self.x = [pos - FloorConsts.FLOOR_VELOCITY for pos in self.x]  # move position left with respect to velocity
+        if abs(self.x[0]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
+            self.x[0] = self.x[3] + FloorConsts.FLOOR_IMAGE.get_width()
+        if abs(self.x[1]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
+            self.x[1] = self.x[0] + FloorConsts.FLOOR_IMAGE.get_width()
+        if abs(self.x[0]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
+            self.x[3] = self.x[2] + FloorConsts.FLOOR_IMAGE.get_width()
+
+
