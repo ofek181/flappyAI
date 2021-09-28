7b185e80ad46573fd04954c9ac8d3048623713c2
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-28 15:07:10 +0300

    changed consts to feel better for the game

--- consts.py
+++ consts.py
@@ -5,12 +5,13 @@ class BirdConsts:
     BIRD_IMAGES = [pygame.image.load('images/bird_wing_middle.png'),
                    pygame.image.load('images/bird_wing_up.png'),
                    pygame.image.load('images/bird_wing_down.png')]
-    GRAVITY = 1.5
+    GRAVITY = 2.5
+    MAX_VELOCITY = 14
     MAX_UP_ANGLE = 45
     MIN_DOWN_ANGLE = -90
     ANGULAR_ACCELERATION = 0.5
-    MIN_ANGULAR_ACCELERATION = 5
-    JUMP_VELOCITY = -10
+    MIN_ANGULAR_ACCELERATION = 7
+    JUMP_VELOCITY = -22
     FLAP_DOWN_ANGLE = -45
 
 
@@ -18,23 +19,27 @@ class PipeConsts:
     BOT_IMAGE = pygame.image.load('images/pipe.png')
     TOP_IMAGE = pygame.transform.flip(BOT_IMAGE, False, True)
     MIN_LENGTH = 100
-    MAX_LENGTH = 300
-    VERTICAL_GAP = 150  # gap between top and bottom pipe
+    MAX_LENGTH = 250
+    VERTICAL_GAP = 130  # gap between top and bottom pipe
     HORIZONTAL_GAP = 200  # gap between pipe pairs on the x axis
-    PIPE_VELOCITY = 5
+    PIPE_STARTING_VELOCITY = 6
+    PIPE_MAX_VELOCITY = 18
+    PIPE_ACCELERATION_X = 0.005
 
 
 class FloorConsts:
     FLOOR_IMAGE = pygame.image.load('images/floor.png')
-    FLOOR_VELOCITY = 5
+    FLOOR_STARTING_VELOCITY = 6
+    FLOOR_MAX_VELOCITY = 18
+    FLOOR_ACCELERATION_X = 0.005
 
 
 class DisplayConsts:
     FPS = 30
-    SCREEN_WIDTH = 900
-    SCREEN_HEIGHT = 600
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 500
     FONT_COLOR = (255, 255, 255)
-    FONT_SIZE = 30
+    FONT_SIZE = 50
     FONT_TYPE = 'comicsansms'
     BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load('images/background.png'),
                                               (SCREEN_WIDTH, SCREEN_HEIGHT))
