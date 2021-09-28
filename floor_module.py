eefcef38c0f5233d1ed1421e1ea70c65b09aaefb
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-28 13:59:21 +0300

    added acceleration

--- floor_module.py
+++ floor_module.py
@@ -21,7 +21,7 @@ class Floor:
            move(self):
                Moves the floor with the velocity of the pipes
     """
-    def __init__(self, y_pos: int = 500):
+    def __init__(self, y_pos: int = 400):
         """
             Constructs all the necessary attributes for the floor object.
                Parameters:
@@ -31,17 +31,21 @@ class Floor:
         self.floor_image = [FloorConsts.FLOOR_IMAGE, FloorConsts.FLOOR_IMAGE, FloorConsts.FLOOR_IMAGE]
         self.x = [0, FloorConsts.FLOOR_IMAGE.get_width(), FloorConsts.FLOOR_IMAGE.get_width() * 2]
         self.y = y_pos  # the y position of the floor image
+        self.velocity = FloorConsts.FLOOR_STARTING_VELOCITY
 
     def move(self):
         """
            Implements the movement of the floor with respect to game velocity.
         """
-        self.x = [pos - FloorConsts.FLOOR_VELOCITY for pos in self.x]  # move position left with respect to velocity
+        self.velocity += FloorConsts.FLOOR_ACCELERATION_X
+        self.velocity = min(self.velocity, FloorConsts.FLOOR_MAX_VELOCITY)
+        self.x = [pos - self.velocity
+                  for pos in self.x]  # move position left with respect to velocity
         if abs(self.x[0]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
-            self.x[0] = self.x[3] + FloorConsts.FLOOR_IMAGE.get_width()
+            self.x[0] = self.x[2] + FloorConsts.FLOOR_IMAGE.get_width()
         if abs(self.x[1]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
             self.x[1] = self.x[0] + FloorConsts.FLOOR_IMAGE.get_width()
         if abs(self.x[0]) > abs(FloorConsts.FLOOR_IMAGE.get_width()):  # if the images move out of screen
-            self.x[3] = self.x[2] + FloorConsts.FLOOR_IMAGE.get_width()
+            self.x[2] = self.x[1] + FloorConsts.FLOOR_IMAGE.get_width()
 
 
