e99e576fd2b59e4130672c9a32330aca6e7f1931
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-28 13:58:25 +0300

    deleted redundent function and added acceleration

--- pipes_module.py
+++ pipes_module.py
@@ -12,12 +12,12 @@ class PipePair:
                x position of the pipe pair on the screen
            pipe_image : list
                list of images for the top and bottom pipes
-           top_pipe_length : int
-               length of the top pipe
-           top_pipe_edge: int
+           top_pipe_head: int
                the upper edge of the top pipe
-           bot_pipe_edge: int
+           bot_pipe_head: int
                the upper edge of the bot pipe
+           velocity: float
+               the velocity of the moving pipe (same as floor)
            ----------------------------
            Methods
            ----------------------------
@@ -25,37 +25,31 @@ class PipePair:
                Constructs all the necessary attributes for the PipePair object.
            move(self):
                Moves the pipe pair at each frame.
-           set_positions(self):
-               Sets random height positions of the pipe pair with fixed vertical gap
     """
-    def __init__(self, x_pos: int = 300):
+    def __init__(self, x_pos: int = 300, velocity: int = PipeConsts.PIPE_STARTING_VELOCITY):
         """
            Constructs all the necessary attributes for the PipePair object.
               Parameters:
                   x_pos (int): x position of the pipe pair
+                  velocity (int) : starting velocity of the instance
         """
         self.x = x_pos  # x position of the pipe pair
         self.pipe_image = []
         self.pipe_image.append(PipeConsts.BOT_IMAGE)  # index 0 represents bottom pipe
         self.pipe_image.append(PipeConsts.TOP_IMAGE)  # index 1 represents top pipe
-        self.top_pipe_length = self.pipe_image[1].get_height() - random.randrange(PipeConsts.MIN_LENGTH,
-                                                                                  PipeConsts.MAX_LENGTH)
-        self.top_pipe_edge = self.top_pipe_length - PipeConsts.BOT_IMAGE.get_height()
-        self.bot_pipe_edge = self.top_pipe_length + PipeConsts.VERTICAL_GAP  # y position of the bottom pipe's edge
+
+        # Setting random height for the pipe pair with fixed vertical gap.
+        self.top_pipe_head = - random.randrange(PipeConsts.MIN_LENGTH, PipeConsts.MAX_LENGTH)
+        self.bot_pipe_head = self.top_pipe_head + self.pipe_image[1].get_height() + PipeConsts.VERTICAL_GAP  # y position of the bottom pipe's edge
+        self.velocity = velocity
 
     def move(self):
         """
-           Implements the movement of the pipe pair.
+           Implements the movement of the pipe pair with acceleration
         """
-        self.x -= PipeConsts.PIPE_VELOCITY
+        self.velocity += PipeConsts.PIPE_ACCELERATION_X
+        self.velocity = min(self.velocity, PipeConsts.PIPE_MAX_VELOCITY)
+        self.x -= self.velocity
 
-    def set_positions(self):
-        """
-           Sets random height for the pipe pair with fixed vertical gap.
-        """
-        self.top_pipe_length = self.pipe_image[1].get_height() - random.randrange(PipeConsts.MIN_LENGTH,
-                                                                                  PipeConsts.MAX_LENGTH)
-        self.top_pipe_edge = self.top_pipe_length - self.pipe_image[1].get_length()
-        self.bot_pipe_edge = self.top_pipe_length + PipeConsts.VERTICAL_GAP  # y position of the bottom pipe's edge
 
 
