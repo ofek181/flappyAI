7554538b5e035f39ffebf68b523077209ae6ec45
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-30 13:00:59 +0300

    changed names and added docstring

--- flappyNEAT/main.py
+++ flappyNEAT/main.py
@@ -3,14 +3,16 @@ from neat_algorithm import NeatAI
 
 
 def main():
-    # Determine path to configuration file. This path manipulation is
-    # here so that the script will run successfully regardless of the
-    # current working directory.
+    """
+        Determine path to configuration file. This path manipulation is
+        here so that the script will run successfully regardless of the
+        current working directory.
+    """
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
-    # NeatAI.run(config_path)
+    NeatAI.train(config_path)  # train
 
-    NeatAI.replay_genome(config_path)
+    # NeatAI.test(config_path) # test
 
 
 if __name__ == '__main__':
