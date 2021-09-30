b62ad0cb3416297e17583dc79f8bd3d324d28f82
Ofek Ophir <ophir.ofek@gmail.com>
2021-09-30 12:39:57 +0300

    added relative pathing

--- flappyGame/consts.py
+++ flappyGame/consts.py
@@ -1,10 +1,12 @@
 import pygame
+import os
+local_dir = os.path.join(os.path.dirname(__file__), "images")
 
 
 class BirdConsts:
-    BIRD_IMAGES = [pygame.image.load('images/bird_wing_middle.png'),
-                   pygame.image.load('images/bird_wing_up.png'),
-                   pygame.image.load('images/bird_wing_down.png')]
+    BIRD_IMAGES = [pygame.image.load(os.path.join(local_dir, 'bird_wing_middle.png')),
+                   pygame.image.load(os.path.join(local_dir, 'bird_wing_up.png')),
+                   pygame.image.load(os.path.join(local_dir, 'bird_wing_down.png'))]
     GRAVITY = 2
     MAX_VELOCITY = 14
     MAX_UP_ANGLE = 45
@@ -16,21 +18,21 @@ class BirdConsts:
 
 
 class PipeConsts:
-    BOT_IMAGE = pygame.image.load('images/pipe.png')
+    BOT_IMAGE = pygame.image.load(os.path.join(local_dir, 'pipe.png'))
     TOP_IMAGE = pygame.transform.flip(BOT_IMAGE, False, True)
     MIN_LENGTH = 100
     MAX_LENGTH = 250
     VERTICAL_GAP = 130  # gap between top and bottom pipe
     HORIZONTAL_GAP = 200  # gap between pipe pairs on the x axis
-    PIPE_STARTING_VELOCITY = 5
-    PIPE_MAX_VELOCITY = 14
+    PIPE_STARTING_VELOCITY = 6
+    PIPE_MAX_VELOCITY = 13
     PIPE_ACCELERATION_X = 0.002
 
 
 class FloorConsts:
-    FLOOR_IMAGE = pygame.image.load('images/floor.png')
-    FLOOR_STARTING_VELOCITY = 5
-    FLOOR_MAX_VELOCITY = 14
+    FLOOR_IMAGE = pygame.image.load(os.path.join(local_dir, 'floor.png'))
+    FLOOR_STARTING_VELOCITY = 6
+    FLOOR_MAX_VELOCITY = 13
     FLOOR_ACCELERATION_X = 0.002
 
 
@@ -44,5 +46,5 @@ class DisplayConsts:
     GAME_OVER_FONT_SIZE = 80
     FONT_TYPE = 'Comic Sans MS'
     GAME_OVER_FONT_TYPE = 'Comic Sans MS'
-    BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load('images/background.png'),
+    BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join(local_dir, 'background.png')),
                                               (SCREEN_WIDTH, SCREEN_HEIGHT))
